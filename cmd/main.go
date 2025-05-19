dc7882fbbd9159e4fa5c22f2fb0c9ad0f5da7996
Kaue Miziara <kauemiziara@gmail.com>
2025-05-19 01:21:29 -0300

    feat: roteamento inicial com Gin e migration das tabelas do banco

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Grupo-Astra/apmd-go-api/config"
+	"github.com/Grupo-Astra/apmd-go-api/migrations"
+	"github.com/Grupo-Astra/apmd-go-api/routes"
 	"github.com/joho/godotenv"
 )
 
@@ -14,5 +19,21 @@ func main() {
 	}
 
 	config.ConnectToOracle()
-	log.Println("Servidor iniciado na porta 8080")
+	defer config.DB.Close()
+
+	migrations.RunMigrations()
+
+	router := routes.SetupRouter()
+
+	go func() {
+		if err := router.Run(":8080"); err != nil {
+			log.Fatalf("Erro ao iniciar o servidor: %v", err)
+		}
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+
+	log.Println("Encerrando aplicação...")
 }
