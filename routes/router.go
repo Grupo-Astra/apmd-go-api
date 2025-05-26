04cce765abbdfc3e6d9fccda96607491f0f0c512
Kaue Miziara <kauemiziara@gmail.com>
2025-05-25 21:03:05 -0300

    config: configura CORS para permitir requisições de `localhost:8081`

--- routes/router.go
+++ routes/router.go
@@ -1,12 +1,24 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/Grupo-Astra/apmd-go-api/handlers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8081"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.GET("/sensors", handlers.GetAllSensors)
 	r.GET("/sensors/:id", handlers.GetSensorByID)
