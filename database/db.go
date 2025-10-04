81969bb200af51ba013ae3808e4a89e5cc4bdfd7
Kaue Miziara <kauemiziara@gmail.com>
2025-10-04 17:18:05 -0300

    feat: adiciona conexão ao PostgreSQL e migraçado dos dados na função `InitDatabase`

--- database/db.go
+++ database/db.go
@@ -2,13 +2,18 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/Grupo-Astra/apmd-go-api/models"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	DB         *gorm.DB
+	PostgresDB *gorm.DB
+)
 
 func InitDatabase() {
 	var err error
@@ -16,9 +21,30 @@ func InitDatabase() {
 	if err != nil {
 		log.Fatal("Erro ao conectar ao SQLite: ", err)
 	}
+	log.Println("Conexão com SQLite estabelecida com sucesso.")
 
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("Variável de ambiente DATABASE_URL não definida.")
+	}
+
+	PostgresDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Erro ao conectar ao PostgresSQL: ", err)
+	}
+	log.Println("Conexão com PostgresSQL estabelecida com sucesso.")
+
+	log.Println("Migrando tabelas para o SQLite...")
 	err = DB.AutoMigrate(&models.Sensor{}, &models.SensorHistory{})
 	if err != nil {
-		log.Fatal("Erro ao migrar tabelas: ", err)
+		log.Fatal("SQLite - Erro ao migrar tabelas: ", err)
 	}
+
+	log.Println("Migrando tabelas para o PostgresSQL...")
+	err = PostgresDB.AutoMigrate(&models.Sensor{}, &models.SensorHistory{})
+	if err != nil {
+		log.Fatal("PostgresSQL - Erro ao migrar tabelas: ", err)
+	}
+
+	log.Println("Tabelas migradas com sucesso em ambos os bancos de dados.")
 }
