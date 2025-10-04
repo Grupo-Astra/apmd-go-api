1fb0aba0af1ccb715dcf66b60187753e84cb5dd6
Kaue Miziara <kauemiziara@gmail.com>
2025-10-04 20:48:59 -0300

    feat: adiciona o método `Update` ao repositório

--- repositories/sensor_repository.go
+++ repositories/sensor_repository.go
@@ -11,6 +11,7 @@ type SensorRepositoryInterface interface {
 	Create(sensor *models.Sensor, history *models.SensorHistory) error
 	FindAll() ([]models.Sensor, error)
 	FindByID(id int) (models.Sensor, error)
+	Update(sensor *models.Sensor, history *models.SensorHistory) error
 }
 
 type sensorRepository struct {
@@ -82,4 +83,49 @@ func (r *sensorRepository) FindByID(id int) (models.Sensor, error) {
 	var sensor models.Sensor
 	err := r.postgresDB.Preload("Historic").First(&sensor, id).Error
 	return sensor, err
+}
+
+func (r *sensorRepository) Update(sensor *models.Sensor, history *models.SensorHistory) error {
+	txPostgres := r.postgresDB.Begin()
+	txSqlite := r.sqliteDB.Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			txPostgres.Rollback()
+			txSqlite.Rollback()
+		}
+	}()
+
+	if err := txPostgres.Save(sensor).Error; err != nil {
+		txPostgres.Rollback()
+		txSqlite.Rollback()
+		return err
+	}
+	if err := txSqlite.Save(sensor).Error; err != nil {
+		txPostgres.Rollback()
+		txSqlite.Rollback()
+		return err
+	}
+
+	history.SensorID = sensor.ID
+	if err := txPostgres.Create(history).Error; err != nil {
+		txPostgres.Rollback()
+		txSqlite.Rollback()
+		return err
+	}
+	if err := txSqlite.Create(history).Error; err != nil {
+		txPostgres.Rollback()
+		txSqlite.Rollback()
+		return err
+	}
+
+	if err := txPostgres.Commit().Error; err != nil {
+		return err
+	}
+	if err := txSqlite.Commit().Error; err != nil {
+		log.Printf("CRITICAL: Falha ao commitar transação no SQLite após sucesso no PostgreSQL: %v", err)
+		return err
+	}
+
+	return nil
 }
